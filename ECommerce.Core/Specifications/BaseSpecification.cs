249d77f4dacb1c952cdb589309550c573324ba94
Omnia Magdy <omnia@example.com>
2026-08-14 15:00:20 +0300

    feat: add ISpecification interface and BaseSpecification class

---
+++ ECommerce.Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+      
+        public Expression<Func<T, bool>> Criteria { get; }
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        public Expression<Func<T, object>> OrderBy { get; private set; } = null!;
+        public Expression<Func<T, object>> OrderByDescending { get; private set; } = null!;
+
+       
+        public BaseSpecification()
+        {
+        }
+
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+
+      
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+      
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
+        }
+    }
+}
---
+++ ECommerce.Repository/DTOs/ProductToReturnDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Repository.DTOs
+{
+    public class ProductToReturnDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; } = null!;
+
+        
+        public string ProductType { get; set; } = null!;
+        public string ProductBrand { get; set; } = null!;
+    }
+}
