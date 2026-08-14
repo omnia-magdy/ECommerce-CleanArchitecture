829ec3f89e646641c808d4be27c26c425d6a30cf
Omnia Magdy <omnia@example.com>
2026-08-14 15:46:31 +0300

    feat: add ProductsWithTypesAndBrandsSpecification

---
+++ ECommerce.Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+      
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(p => p.ProductBrand);
+            AddInclude(p => p.ProductType);
+        }
+
+        
+        public ProductsWithTypesAndBrandsSpecification(int id)
+            : base(p => p.Id == id) 
+        {
+            AddInclude(p => p.ProductBrand);
+            AddInclude(p => p.ProductType);
+        }
+    }
+}
